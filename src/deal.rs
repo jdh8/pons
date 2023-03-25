2af85259b5ada84105667482058e2231e56c2251
Chen-Pang He <chen.pang.he@jdh8.org>
2023-03-26 02:55:28 +0800

    Bitset operators for Holding

--- src/deal.rs
+++ src/deal.rs
@@ -1,6 +1,6 @@
 use rand::prelude::SliceRandom;
 use core::fmt;
-use core::ops::{BitAnd, BitOr, BitXor, Index, IndexMut, Sub, Not};
+use core::ops::{BitAnd, BitOr, BitXor, Index, IndexMut, Not, Sub};
 
 #[derive(Clone, Copy, Debug)]
 #[repr(u8)]
@@ -101,6 +101,46 @@ impl Holding {
 
     pub fn from_bits(bits: u16) -> Self {
         Self(bits & Self::ALL)
+    }
+}
+
+impl BitAnd for Holding {
+    type Output = Self;
+
+    fn bitand(self, rhs: Self) -> Self {
+        Self(self.0 & rhs.0)
+    }
+}
+
+impl BitOr for Holding {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self {
+        Self(self.0 | rhs.0)
+    }
+}
+
+impl BitXor for Holding {
+    type Output = Self;
+
+    fn bitxor(self, rhs: Self) -> Self {
+        Self(self.0 ^ rhs.0)
+    }
+}
+
+impl Not for Holding {
+    type Output = Self;
+
+    fn not(self) -> Self {
+        Self::from_bits(!self.0)
+    }
+}
+
+impl Sub for Holding {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self(self.0 & !rhs.0)
     }
 }
 
