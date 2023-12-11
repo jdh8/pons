acdcda8c234bea5784565600263dc84b510b3655
Chen-Pang He <chen.pang.he@jdh8.org>
2023-12-12 01:09:16 +0800

    Experiment on total notrump tricks

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,36 @@ mod dds;
 mod eval;
 mod test;
 
-fn analyze_deals(n: usize) {
-    let deals: Vec<deal::Deal> = (0..n).map(|_| deal::shuffled_standard_52_deck().deal()).collect();
-    let solutions = dds::solve(&deals, dds::StrainFlags::all());
+#[derive(Clone, Copy, Debug, Default)]
+struct Accumulator {
+    ns: usize,
+    ew: usize,
+    total: usize,
+}
 
-    for (deal, sol) in deals.iter().zip(solutions) {
-        println!("{deal} {sol}");
-    }
+fn analyze_deals(n: usize) {
+    let deals: Vec<_> = (0..n).map(|_| deal::shuffled_standard_52_deck().deal()).collect();
+    let solution = dds::solve(&deals, dds::StrainFlags::NOTRUMP)
+        .into_iter()
+        .map(|table| table[contract::Strain::Notrump])
+        .fold(Accumulator::default(), |mut acc, sol| {
+            let (n, e, s, w) = (
+                sol.at(deal::Seat::North),
+                sol.at(deal::Seat::East),
+                sol.at(deal::Seat::South),
+                sol.at(deal::Seat::West),
+            );
+            acc.ns += usize::from(core::cmp::max(n, s));
+            acc.ew += usize::from(core::cmp::max(e, w));
+            acc.total += usize::from(n + e + s + w);
+            acc
+        });
+
+    dbg!(solution);
 }
 
 fn main() {
     std::env::args().nth(1).map_or_else(
         || analyze_deals(100),
-         |string| string.parse::<usize>().map_or_else(|_| todo!(), analyze_deals));
+        |string| string.parse::<usize>().map_or_else(|_| todo!(), analyze_deals));
 }
