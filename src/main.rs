6a9eb49556123d6569ef9c09f361768761958eba
Chen-Pang He <chen.pang.he@jdh8.org>
2024-06-05 06:09:16 +0800

    Remove code covered by dds-bridge

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-mod contract;
-mod deal;
-mod dds;
-mod eval;
-mod test;
+use dds_bridge::contract::Strain;
+use dds_bridge::deal::{Deal, Seat};
+use dds_bridge::solver;
 
 #[derive(Clone, Copy, Debug, Default)]
 struct Accumulator {
@@ -11,17 +9,20 @@ struct Accumulator {
     total: usize,
 }
 
-fn analyze_deals(n: usize) {
-    let deals: Vec<_> = (0..n).map(|_| deal::shuffled_standard_52_deck().deal()).collect();
-    let solution = dds::solve(&deals, dds::StrainFlags::NOTRUMP)
+fn analyze_deals(n: usize) -> Result<(), solver::Error> {
+    let deals: Vec<_> = core::iter::repeat_with(|| Deal::new(&mut rand::thread_rng()))
+        .take(n)
+        .collect();
+
+    let solution = solver::solve_deals(&deals, solver::StrainFlags::NOTRUMP)?
         .into_iter()
-        .map(|table| table[contract::Strain::Notrump])
+        .map(|table| table[Strain::Notrump])
         .fold(Accumulator::default(), |mut acc, sol| {
             let (n, e, s, w) = (
-                sol.at(deal::Seat::North),
-                sol.at(deal::Seat::East),
-                sol.at(deal::Seat::South),
-                sol.at(deal::Seat::West),
+                sol.at(Seat::North),
+                sol.at(Seat::East),
+                sol.at(Seat::South),
+                sol.at(Seat::West),
             );
             acc.ns += usize::from(n.max(s));
             acc.ew += usize::from(e.max(w));
@@ -30,10 +31,11 @@ fn analyze_deals(n: usize) {
         });
 
     dbg!(solution);
+    Ok(())
 }
 
-fn main() {
+fn main() -> Result<(), solver::Error> {
     std::env::args().nth(1).map_or_else(
         || analyze_deals(100),
-        |string| string.parse::<usize>().map_or_else(|_| todo!(), analyze_deals));
+        |string| string.parse::<usize>().map_or_else(|_| todo!(), analyze_deals))
 }
