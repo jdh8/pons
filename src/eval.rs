334816c2c10c8c4005da5721df287354a5dfdeeb
Chen-Pang He <chen.pang.he@jdh8.org>
2024-06-12 03:54:36 +0800

    Remove outdated code

--- src/eval.rs
+++ src/eval.rs
@@ -1,5 +1,4 @@
-use crate::contract::Strain;
-use crate::deal::{Holding, Hand, SmallSet};
+use dds_bridge::deal::{Hand, Holding, SmallSet as _};
 
 pub trait HandEvaluator {
     fn eval(&self, hand: Hand) -> f64;
@@ -9,28 +8,26 @@ pub struct SimpleEvaluator<F: Fn(Holding) -> f64>(F);
 
 impl<F: Fn(Holding) -> f64> HandEvaluator for SimpleEvaluator<F> {
     fn eval(&self, hand: Hand) -> f64 {
-        self.0(hand[Strain::Clubs]) +
-        self.0(hand[Strain::Diamonds]) +
-        self.0(hand[Strain::Hearts]) +
-        self.0(hand[Strain::Spades])
+        hand.0.map(&self.0).into_iter().sum()
     }
 }
 
 fn hcp(holding: Holding) -> f64 {
     f64::from(
-        4 * i32::from(holding.contains(14)) +
-        3 * i32::from(holding.contains(13)) +
-        2 * i32::from(holding.contains(12)) +
-        i32::from(holding.contains(11)))
+        4 * i32::from(holding.contains(14))
+            + 3 * i32::from(holding.contains(13))
+            + 2 * i32::from(holding.contains(12))
+            + i32::from(holding.contains(11)),
+    )
 }
 
 fn fifths(holding: Holding) -> f64 {
-    (if holding.contains(14) { 4.0 } else { 0.0 } +
-     if holding.contains(13) { 2.8 } else { 0.0 } +
-     if holding.contains(12) { 1.8 } else { 0.0 } +
-     if holding.contains(11) { 1.0 } else { 0.0 } +
-     if holding.contains(10) { 0.4 } else { 0.0 })
+    (if holding.contains(14) { 4.0 } else { 0.0 }
+        + if holding.contains(13) { 2.8 } else { 0.0 }
+        + if holding.contains(12) { 1.8 } else { 0.0 }
+        + if holding.contains(11) { 1.0 } else { 0.0 }
+        + if holding.contains(10) { 0.4 } else { 0.0 })
 }
 
-const HCP: SimpleEvaluator<fn(Holding) -> f64> = SimpleEvaluator(hcp);
-const FIFTHS: SimpleEvaluator<fn(Holding) -> f64> = SimpleEvaluator(fifths);
+pub const HCP: SimpleEvaluator<fn(Holding) -> f64> = SimpleEvaluator(hcp);
+pub const FIFTHS: SimpleEvaluator<fn(Holding) -> f64> = SimpleEvaluator(fifths);
