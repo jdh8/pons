85fb32ead3ed9b95e1e4785d04cb34cd10cc2203
Chen-Pang He <chen.pang.he@jdh8.org>
2024-06-13 11:32:00 +0800

    Add shortness points for suit contracts

--- src/eval.rs
+++ src/eval.rs
@@ -10,7 +10,7 @@ pub trait HandEvaluator<T> {
 }
 
 /// Evaluator summing values of suit holdings
-pub struct SimpleEvaluator<T: Sum, F: Fn(Holding) -> T>(F);
+pub struct SimpleEvaluator<T: Sum, F: Fn(Holding) -> T>(pub F);
 
 impl<T: Sum, F: Fn(Holding) -> T> HandEvaluator<T> for SimpleEvaluator<T, F> {
     fn call(&self, hand: Hand) -> T {
@@ -27,6 +27,13 @@ pub fn hcp(holding: Holding) -> i32 {
         + 3 * i32::from(holding.contains(13))
         + 2 * i32::from(holding.contains(12))
         + i32::from(holding.contains(11))
+}
+
+/// Short suit points
+#[must_use]
+#[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
+pub fn shortness(holding: Holding) -> i32 {
+    3 - holding.len().max(3) as i32
 }
 
 /// The [Fifths] evaluator for 3NT, &times; 10
@@ -61,7 +68,7 @@ pub fn centi_bumrap(holding: Holding) -> i32 {
 #[must_use]
 pub fn ltc(holding: Holding) -> i32 {
     let len = holding.len();
-    
+
     i32::from(len >= 1 && !holding.contains(14))
         + i32::from(len >= 2 && !holding.contains(13))
         + i32::from(len >= 3 && !holding.contains(12))
@@ -73,7 +80,7 @@ pub fn ltc(holding: Holding) -> i32 {
 #[must_use]
 pub fn half_nltc(holding: Holding) -> i32 {
     let len = holding.len();
-    
+
     3 * i32::from(len >= 1 && !holding.contains(14))
         + 2 * i32::from(len >= 2 && !holding.contains(13))
         + i32::from(len >= 3 && !holding.contains(12))
@@ -84,6 +91,14 @@ pub fn half_nltc(holding: Holding) -> i32 {
 /// This is the well-known 4-3-2-1 point count by Milton Work.  Evaluation of
 /// each suit is done by [`hcp`].
 pub const HCP: SimpleEvaluator<i32, fn(Holding) -> i32> = SimpleEvaluator(hcp);
+
+/// High card points plus useful shortness
+///
+/// For each suit, we take the maximum of high card points and shortness.  This
+/// method avoids double counting of short honors.  This evaluator is
+/// particularly useful for suit contracts.
+pub const HCP_SHORTNESS: SimpleEvaluator<i32, fn(Holding) -> i32> =
+    SimpleEvaluator(|x| hcp(x).max(shortness(x)));
 
 /// The [Fifths] evaluator for 3NT, &times; 10
 ///
@@ -100,6 +115,14 @@ pub const DECI_FIFTHS: SimpleEvaluator<i32, fn(Holding) -> i32> = SimpleEvaluato
 /// make the result an integer to improve interoperability.  This evaluator
 /// calls [`centi_bumrap`] for each suit.
 pub const CENTI_BUMRAP: SimpleEvaluator<i32, fn(Holding) -> i32> = SimpleEvaluator(centi_bumrap);
+
+/// BUM-RAP with shortness, &times; 100
+///
+/// For each suit, we take the maximum of BUM-RAP and shortness.  This method
+/// avoids double counting of short honors.  This evaluator is particularly
+/// useful for suit contracts.
+pub const BUMRAP_SHORTNESS: SimpleEvaluator<i32, fn(Holding) -> i32> =
+    SimpleEvaluator(|x| centi_bumrap(x).max(100 * shortness(x)));
 
 /// Plain old losing trick count
 pub const LTC: SimpleEvaluator<i32, fn(Holding) -> i32> = SimpleEvaluator(ltc);
