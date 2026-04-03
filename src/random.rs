843b2b79e829b6fd5be3f7d4e4da874bcc3d33d7
Chen-Pang He <chen.pang.he@jdh8.org>
2026-04-04 03:46:30 +0800

    First implementation of `fill_deals`

--- src/random.rs
+++ src/random.rs
@@ -1,8 +1,66 @@
 use core::num::Wrapping;
 use dds_bridge::contract::{Bid, Contract, Penalty, Strain};
-use dds_bridge::deal::{Deal, Hand, Seat, SmallSet};
+use dds_bridge::deal::{Deal, Hand, Seat, SmallSet as _};
 use dds_bridge::deck::Deck;
-use dds_bridge::solver::{self, Error, StrainFlags, Vulnerability};
+use dds_bridge::solver::{self, Error, StrainFlags, SystemError, Vulnerability};
+use rand::Rng;
+
+pub use dds_bridge::deck::full_deal;
+
+/// Given existing cards in a deal, randomly fill the remaining cards.
+///
+/// - `deal`: The initial deal with some cards already assigned.
+/// - `count`: The number of deals to generate.
+/// - `filter`: An optional function to filter deals.
+///
+/// # Errors
+///
+/// [`dds_bridge::solver::SystemError`] if `deal` is invalid, such as
+///
+/// - Any hand having more than 13 cards.
+/// - Duplicate cards across hands.
+pub fn fill_deals(
+    rng: &mut (impl Rng + ?Sized),
+    deal: &Deal,
+    count: usize,
+    filter: Option<impl FnMut(&Deal) -> bool>,
+) -> Result<Vec<Deal>, SystemError> {
+    if deal.0.into_iter().any(|hand| hand.len() > 13) {
+        return Err(SystemError::TooManyCards);
+    }
+    if deal.0.into_iter().reduce(Hand::intersection) != Some(Hand::EMPTY) {
+        return Err(SystemError::DuplicateCards);
+    }
+
+    let deck = Deck::from(
+        deal.0
+            .into_iter()
+            .fold(Hand::ALL, Hand::symmetric_difference),
+    );
+
+    #[allow(clippy::missing_panics_doc)]
+    let shortest = Seat::ALL
+        .into_iter()
+        .min_by_key(|&seat| deal[seat].len())
+        .expect("Seat::ALL shall not be empty");
+
+    let repeat = core::iter::repeat_with(|| {
+        let mut deck = deck.clone();
+        let mut deal = *deal;
+
+        for i in 1..=3 {
+            let hand = &mut deal[shortest + Wrapping(i)];
+            *hand = *hand | deck.partial_shuffle(rng, 13 - hand.len());
+        }
+        deal[shortest] = deal[shortest] | deck.collect();
+        deal
+    });
+
+    Ok(match filter {
+        Some(f) => repeat.filter(f).take(count).collect(),
+        None => repeat.take(count).collect(),
+    })
+}
 
 /// Emulate `n` deals and calculate par for the NS pair
 ///
