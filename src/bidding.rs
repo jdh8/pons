60378399278257046044427039290ab2d06f2bb0
Chen-Pang He <chen.pang.he@jdh8.org>
2026-04-22 00:37:12 +0800

    Move serde submodules to new files

--- src/bidding.rs
+++ src/bidding.rs
@@ -5,6 +5,9 @@ pub mod map;
 /// [`Trie`] as a bidding system
 pub mod trie;
 
+#[cfg(feature = "serde")]
+mod serde_;
+
 pub use array::Array;
 pub use map::Map;
 pub use trie::Trie;
@@ -447,50 +450,6 @@ impl System for trie::Forest {
         auction: &[Call],
     ) -> Option<array::Logits> {
         self[vul].classify(hand, vul, auction)
-    }
-}
-
-#[cfg(feature = "serde")]
-mod serde_string {
-    use super::{Auction, Call};
-    use core::fmt::Display;
-    use core::str::FromStr;
-    use serde::{Deserialize, Deserializer, Serialize, Serializer, de};
-
-    fn serialize<T: Display, S: Serializer>(value: &T, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.collect_str(value)
-    }
-
-    fn deserialize<'de, T, D>(deserializer: D) -> Result<T, D::Error>
-    where
-        T: FromStr,
-        T::Err: Display,
-        D: Deserializer<'de>,
-    {
-        let s = <&str>::deserialize(deserializer)?;
-        s.parse().map_err(de::Error::custom)
-    }
-
-    impl Serialize for Call {
-        fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-            serialize(self, s)
-        }
-    }
-    impl<'de> Deserialize<'de> for Call {
-        fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
-            deserialize(d)
-        }
-    }
-
-    impl Serialize for Auction {
-        fn serialize<S: Serializer>(&self, s: S) -> Result<S::Ok, S::Error> {
-            serialize(self, s)
-        }
-    }
-    impl<'de> Deserialize<'de> for Auction {
-        fn deserialize<D: Deserializer<'de>>(d: D) -> Result<Self, D::Error> {
-            deserialize(d)
-        }
     }
 }
 
